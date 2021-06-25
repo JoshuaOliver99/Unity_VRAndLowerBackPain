81ba0b2261c82697fe6c19f09f75b8dbed22485f
Joshua Simpson <56636418+JoshuaOliver99@users.noreply.github.com>
2021-06-25 14:21:11 +0100

    Contined updating avatar movement
    - Added .log to gitignore
    - Added test location cubes
    - Neatened project files
    - identified inverse avatar movement (hips + sibling transforms shifting)

--- VR and lower back pain/Assets/_App/Scripts/VRRig.cs
+++ VR and lower back pain/Assets/_App/Scripts/VRRig.cs
@@ -19,22 +19,26 @@ public class VRMap
 
 public class VRRig : MonoBehaviour
 {
-    [Header("Debug")]
-    [SerializeField] bool Head = true;
-    [SerializeField] bool LHand = true;
-    [SerializeField] bool RHand = true;
-    [SerializeField] bool Hips = true;
+    [Header("Debug - Tick if used")]
+    [SerializeField] bool useHead = true;
+    [SerializeField] bool useLHand = true;
+    [SerializeField] bool useRHand = true;
+    [SerializeField] bool useHips = true;
+    [SerializeField] bool useLFoot = true;
+    [SerializeField] bool useRFoot = true;
 
     [Header("Settings")]
     [SerializeField] float turnSmoothness;
 
-    [SerializeField] raaTrackers RaaTrackers;
+    [SerializeField] raaTrackers RaaTrackers; // Reference to detected Trackers
 
     [Header("Constraint settings")]
     [SerializeField] VRMap head;
     [SerializeField] VRMap leftHand;
     [SerializeField] VRMap rightHand;
     [SerializeField] VRMap hips;
+    [SerializeField] VRMap leftFoot;
+    [SerializeField] VRMap rightFoot;
 
     [SerializeField] Transform headConstraint;
     [SerializeField] Vector3 headBodyOffset;
@@ -44,8 +48,8 @@ public class VRRig : MonoBehaviour
     {
         headBodyOffset = transform.position - headConstraint.position;
 
-        if (RaaTrackers == null)
-            RaaTrackers = GameObject.FindObjectOfType<raaTrackers>();
+        //if (RaaTrackers ==null)
+            //RaaTrackers = GameObject.FindObjectOfType<raaTrackers>();
 
         // Get Tracker targets
         //head.vrTarget = RaaTrackers.trackerHead.transform;
@@ -62,9 +66,11 @@ public class VRRig : MonoBehaviour
         // Update head position smoothly
         transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
 
-        if (Head) head.Map();
-        if (LHand) leftHand.Map();
-        if (RHand) rightHand.Map();
-        if (Hips) hips.Map();
+        if (useHead)    head.Map();
+        if (useLHand)   leftHand.Map();
+        if (useRHand)   rightHand.Map();
+        if (useHips)    hips.Map();     // NOTE: Maybe undeeded implementation for hips. Maybe .position is enough
+        if (useLFoot)   leftFoot.Map();
+        if (useRFoot)   rightFoot.Map();
     }
 }
